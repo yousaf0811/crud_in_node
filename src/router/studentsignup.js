4cbe66a4f1f1a078753343188d61e5b19caaa242
M. Yousaf Ramzan <yousaf0811@gmail.com>
2022-12-31 12:15:49 +0000

    2nd

--- src/router/studentsignup.js
+++ src/router/studentsignup.js
@@ -1,6 +1,45 @@
 const express = require("express");
 const studentdata = require("../model/users")
 const router = express.Router();
+const {generateAuthToken} = require('../utils/helpers')
+
+
+router.get("/get", async (req, res) => {
+  const students = await studentdata.find();
+  res.status(200).send(students);
+});
+
+router.get("/:userId", async (req, res) => {
+  const user = await studentdata.findOne({ _id: req.params.userId });
+  res.status(200).send(user);
+});
+
+
+router.post("/login", async (req, res) => {
+  const user = await studentdata.findOne({ email: req.body.email });
+  const pass = await studentdata.findOne({password: req.body.password });
+
+  if (!user) {
+    return res.status(400).send({ message: "Invalid Email or Password" });
+  }
+
+  if (!pass) {
+    return res.status(400).send({ message: "Invalid Email or Password" });
+  }
+
+  const token = generateAuthToken({
+    username: user.username,
+    email: user.email
+  });
+  const data = {
+    username: user.username,
+    email: user.email
+  }
+  console.log("this is your tokens",token)
+
+  res.status(200).send({ message: "success", token, data });
+});
+
 
 
 
